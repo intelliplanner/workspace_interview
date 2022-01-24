46332e9359d39ad185f5cb727eb600757d5cbe4f
Virendra Gupta <virendra.gupta@uneecops.com>
2022-01-24 22:24:27 +0530

    test

--- SpringWithMySqlAndJPACrud/src/main/java/com/javatechie/test/SpringWithMySqlAndJpaCrudApplication.java
+++ SpringWithMySqlAndJPACrud/src/main/java/com/javatechie/test/SpringWithMySqlAndJpaCrudApplication.java
@@ -1,11 +1,24 @@
 package com.javatechie.test;
 
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringWithMySqlAndJpaCrudApplication {
 
+	Logger log = LoggerFactory.getLogger(SpringWithMySqlAndJpaCrudApplication.class);
+			
+	@PostConstruct
+	public void init() {
+		log.info("Application Started");
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWithMySqlAndJpaCrudApplication.class, args);
 	}
--- SpringWithMySqlAndJPACrud/src/test/java/com/javatechie/test/SpringWithMySqlAndJpaCrudApplicationTests.java
+++ SpringWithMySqlAndJPACrud/src/test/java/com/javatechie/test/SpringWithMySqlAndJpaCrudApplicationTests.java
@@ -1,13 +1,19 @@
 package com.javatechie.test;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringWithMySqlAndJpaCrudApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true, true);
 	}
 
 }
