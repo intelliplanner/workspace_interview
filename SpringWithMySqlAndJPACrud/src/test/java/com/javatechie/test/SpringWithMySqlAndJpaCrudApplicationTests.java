18fb7aaf86898a214a64d1291b9341c915a64c11
Virendra Gupta <virendra.gupta@uneecops.com>
2022-01-25 00:35:16 +0530

    test

--- SpringWithMySqlAndJPACrud/src/test/java/com/javatechie/test/SpringWithMySqlAndJpaCrudApplicationTests.java
+++ SpringWithMySqlAndJPACrud/src/test/java/com/javatechie/test/SpringWithMySqlAndJpaCrudApplicationTests.java
@@ -15,7 +15,7 @@ class SpringWithMySqlAndJpaCrudApplicationTests {
 	Logger log = LoggerFactory.getLogger(SpringWithMySqlAndJpaCrudApplication.class);
 
 	@Test
-	void contextLoads() {
+	 contextLoads() {
 		log.info("Application loaded....");
 		log.info("Virendra Test loaded....");
 		assertEquals(true, true);
